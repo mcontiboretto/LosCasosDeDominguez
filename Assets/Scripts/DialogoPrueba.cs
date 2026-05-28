48d1840b78fea6adf5747f2b821ad778798ae6e8
godoymacielcandelaria-tech <godoymaciel.candelaria@usal.edu.ar>
2026-05-28 10:50:15 -0300

    ScriptSistemaDialogo

---
+++ Assets/Scripts/DialogoPrueba.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogoPrueba : MonoBehaviour
+
+
+{
+    public void Start()
+    {
+        //cajaTexto.text = dialogo.dialogo[0];
+        //img.sprite = dialogo.img;   
+    }
+
+    public DialogoSistema dialogo;
+    public TextMeshProUGUI cajaTexto;
+    public Image img;
+
+
+
+
+}
---
+++ Assets/Scripts/DialogoSistema.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+[CreateAssetMenu(fileName = "Objeto", menuName = "NuevoDialogo")]
+public class DialogoSistema : ScriptableObject 
+    
+    
+{
+    //public string nombre;
+    //[TextAreaAttribute(3, 10)] public  string [] dialogo;
+    //public Sprite img;
+    public Dialog[] dialogos;
+
+}
+
+[System.Serializable]
+public class Dialog
+{
+    [TextAreaAttribute(3, 10)] public string dialogo;
+    public string nombre;
+    public Sprite cuadrodialogo;
+    public Sprite personaje;
+    public Sprite fondo;
+    public Button botoncerrar;
+
+}
